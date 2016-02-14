e43a7c85117bad06ce745f6e027dc4bcc4ce0d59
Sally Kong <kongsally@users.noreply.github.com>
2016-02-14 16:39:39 -0500

    Pie charts for keyword filtered sex,alcohol,drug fb posts

--- status_reader.py
+++ status_reader.py
@@ -9,7 +9,7 @@ import json
 
 plotly_id = os.environ['PLOTLY_ID']
 plotly_api = os.environ['PLOTLY_API_KEY']
-print plotly_id, plotly_api
+# print plotly_id, plotly_api
 py.sign_in(plotly_id, plotly_api)
 
 
@@ -23,9 +23,10 @@ def xldate_to_datetime(xldate):
 
 
 def load_statuses(dates, statuses, users):
-	with open('data/statuses.csv', 'rb') as csvfile:
-		status_reader = csv.reader(csvfile.read().splitlines())
+	with open('training_data.tsv', 'rb') as tsvfile:
+		status_reader = tsvfile.read().splitlines()
 		for row in status_reader:
+			row = row.split('\t')
 			formatted_date = xldate_to_datetime(float(row[4]))
 			
 			if formatted_date not in dates:
@@ -43,9 +44,9 @@ def load_statuses(dates, statuses, users):
 
 
 def print_statuses(status_file_name, statuses, keywords, keyword_stats):
-	status_count_file = open(status_file_name + "_count", 'w')
-	status_file = open(status_file_name, 'w')
-
+	status_count_file = open(status_file_name + "_count.tsv", 'w')
+	status_file = open(status_file_name + ".tsv", 'w')
+	
 	for user_id in statuses.keys():
 		for stat in statuses[user_id]:
 			if any(word in stat.split(" ") for word in keywords):
@@ -97,6 +98,106 @@ def status_bargraph(y, formatted_dates, user_ids, users):
 	fig = go.Figure(data=data, layout=layout)
 	plot_url = py.plot(fig, filename='stat_count_year')
 
+def status_piechart(sex, alcohol, drug) :
+
+	total_stat_count = {}
+	for user in sex.keys():
+		total_stat_count[user] = [len(sex[user]), 0, 0]
+
+	for user in alcohol.keys():
+		if user in total_stat_count.keys():
+			total_stat_count[user][1] = len(alcohol[user])
+		else:
+			total_stat_count[user] = [0, 1, 0]
+
+	for user in drug.keys():
+		if user in total_stat_count.keys():
+			total_stat_count[user][2] = len(drug[user])
+		else:
+			total_stat_count[user] = [0, 0, 1]
+
+	total_userIDs = total_stat_count.keys()
+	sex_counts = []
+	alcohol_counts = []
+	drug_counts = []
+
+	for i in range(0, len(total_userIDs)):
+		sex_counts.append(total_stat_count[total_userIDs[i]][0])
+		alcohol_counts.append(total_stat_count[total_userIDs[i]][1])
+		drug_counts.append(total_stat_count[total_userIDs[i]][2])
+
+	sex_sum = "Sex: " + str(sum(sex_counts))
+	alcohol_sum = "Alcohol: " + str(sum(alcohol_counts))
+	drug_sum = "Drug: " + str(sum (drug_counts)) 
+
+	fig = {
+		"data": [
+			{
+				"values" : sex_counts,
+				"labels" : total_userIDs,
+				"name" : "Sex",
+				"hole": .4,
+     			"type": "pie",
+     			"domain": {"x": [0, .31]},
+     			'textinfo':'none'
+			},
+			{
+				"values" : alcohol_counts,
+				"labels" : total_userIDs,
+				"name" : "Alcohol",
+				"hole": .4,
+      			"type": "pie",
+      			"domain": {"x": [0.33, .64]},
+      			'textinfo':'none'
+			},
+			{
+				"values" : drug_counts,
+				"labels" : total_userIDs,
+				"name" : "Drug",
+				"hole": .4,
+      			"type": "pie",
+      			"domain": {"x": [0.66, 1]},
+      			'textinfo':'none'
+			}
+		],
+		"layout" : {
+			"title": "Number of Facebook Statuses with Sex, Alcohol, or Drug Content",
+			"annotations": [
+            {
+                "font": {
+                    "size": 11
+                },
+                "showarrow": False,
+                "text": sex_sum,
+                "x": 0.12,
+                "y": 0.5
+            },
+            {
+                "font": {
+                    "size": 11
+                },
+                "showarrow": False,
+                "text": alcohol_sum,
+                "x": 0.49,
+                "y": 0.5
+            },
+            {
+                "font": {
+                    "size": 11
+                },
+                "showarrow": False,
+                "text": drug_sum,
+                "x": 0.87,
+                "y": 0.5
+            }]
+
+		}
+	}
+
+	plot_url = py.plot(fig, filename='stat_by_content')
+
+
+
 
 def main():
 
@@ -110,19 +211,24 @@ def main():
 		stat_lengths[user] = len(statuses[user])
 
 	user_by_freq = sorted(stat_lengths, key=stat_lengths.get, reverse=True)
-	
+	for x in user_by_freq:
+		print x + '\t' + str(stat_lengths[x])
+	print len(user_by_freq)
 
-	# sex_statuses = {}
-	# sex_keywords=["sex", "pussy", "naked", "penis", "rape"]
-	# print_statuses("sexual_statuses", statuses, sex_keywords, sex_statuses)
 
-	# alcohol_statuses = {}
-	# alcohol_keywords=["drunk", "beer", "rum", "vodka", "tequila"]
-	# print_statuses("alcohol_statuses", statuses, alcohol_keywords, alcohol_statuses)
+	sex_statuses = {}
+	sex_keywords=["sex", "pussy", "naked", "penis", "rape"]
+	print_statuses("sex", statuses, sex_keywords, sex_statuses)
 
-	# drug_statuses = {}
-	# drug_keywords=["marijuana", "weed", "cocaine", "crackhead", "smoke"]
-	# print_statuses("drug_statuses", statuses, drug_keywords, drug_statuses)
+	alcohol_statuses = {}
+	alcohol_keywords=["drunk", "beer", "wasted", "vodka", "tequila"]
+	print_statuses("alcohol", statuses, alcohol_keywords, alcohol_statuses)
+
+	drug_statuses = {}
+	drug_keywords=["marijuana", "weed", "cocaine", "crackhead", "smoke"]
+	print_statuses("drug", statuses, drug_keywords, drug_statuses)
+
+	status_piechart(sex_statuses, alcohol_statuses, drug_statuses)
 
 	formatted_dates = sorted(dates)
 	z = []
@@ -139,7 +245,7 @@ def main():
 	# status_heatmap(users.keys(), formatted_dates, z)
 
 	#for stacked bargraph
-	status_bargraph(users.keys(), formatted_dates, user_by_freq, users)
+	# status_bargraph(users.keys(), formatted_dates, user_by_freq, users)
 
 
 if __name__ == "__main__":
